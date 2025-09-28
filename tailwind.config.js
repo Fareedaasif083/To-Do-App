96a289c32afc48b150e98ca26ea78d62fd88351b
Fareeda Asif <fareedaasif8130@gmail.com>
2025-09-29 03:38:08 +0500

    Integrated Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./add-task.html", "./src/**/*.{ts,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
